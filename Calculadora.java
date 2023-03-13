71d701f98d6062cb4d96e8d969c279a4c0354457
Daísy Maceno <117369460+daisy-maceno@users.noreply.github.com>
2023-03-12 21:19:30 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,84 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		int divisao = divisao(a,b);
+		
+		System.out.println("soma " + soma);
+		System.out.println("subtracao " + subtracao);
+		System.out.println("mult " + multiplicacao);
+		System.out.println("div " + divisao);
+	
+	
+
+		
+
+	int subtracao1(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static int soma(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static int multiplicacao(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+		
+	}
+
+	public static int soma(int a, b) {
+		return a + b;
+	}
+	
+	public static int subtracao(int a, b) {
+		return a - b;
+	}
+	
+	public static int divisao(int a, int b, b) {
+		return a / b;
+	}
+	
+	public static int mutiplicacao(int a, b) {
+		return a * b;
+		}
+}
+
+	private static int divisao(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static int multiplicacao(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static int subtracao(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static int soma(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
+
